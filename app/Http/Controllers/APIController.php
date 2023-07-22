086f8e9b57cf509e51127182ee5faa44aa1901d8
Ash <vetur0220@gmail.com>
2023-07-22 17:33:49 +0800

    add swagger

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+* @OA\Info(
+ *      version="1.0.0",
+ *      title="Ash TestAPI",
+ *      description="",
+ *      @OA\Contact(
+ *          email="vetur0220@gmail.com"
+ *      )
+ * )
+ *  * @OA\Get(
+ *     path="/TestAPI",
+ *     @OA\Response(response="200", description="Display a listing of test!")
+ * )
+ 
+ */
+class APIController extends Controller
+{
+   
+}
